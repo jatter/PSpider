fc579d341da2f37e2761d3fed673dc8d98f9badd
lijie <jie.li@imgchina.com.cn>
2016-08-05 23:37:02 +0800

    base

---
+++ action/testcase.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+#coding=utf-8
+import web,time
+from action.base import base as baseAction
+import model
+class testcase(baseAction):
+    def __init__(self):
+        if self.isLogin() != True:
+            raise web.seeother('/')
+        baseAction.__init__(self)
+        settings = self.getSettings()
+        self.assignTplDir(settings.ADMIN_TPL_DIR)
+    def save(self):
+        userInput = self.getInput()
+        date = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+        data={
+            'CASE_NAME':userInput['casename'],
+			'PAGE_URL':userInput['pageurl'],
+            'CASE_CONTENT':self.htmlunquote(userInput['content']),
+			'FILE_NAME':userInput['filename'],
+            'CREATE_TIME':date
+        }
+        print data
+        status = model.testcase().insert(data)
+        return self.success('保存成功') if status else self.error('保存失败')
+    def add(self):
+        print "===add======"
+        return self.display('caseAdd')
+	
